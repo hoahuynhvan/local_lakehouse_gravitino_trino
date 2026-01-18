b06ec5141dbc0ea28ecd12258f76f77dead455db
Hoa Huynh <hoahuynh@Hoas-MacBook-Pro.local>
2026-01-18 19:05:34 +0700

    Initial commit: clean and lightweight

---
+++ trino_config/coordinator/init.sql
@@ -0,0 +1,6 @@
+CREATE SCHEMA IF NOT EXISTS catalog_iceberg.landing;
+CREATE SCHEMA IF NOT EXISTS catalog_iceberg.staging;
+CREATE SCHEMA IF NOT EXISTS catalog_iceberg.curated;
+-- CREATE USER admin WITH PASSWORD 'password';
+-- CREATE DATABASE lakehouse WITH OWNER = admin ENCODING = 'UTF8';
+-- GRANT ALL PRIVILEGES ON DATABASE lakehouse TO admin;
